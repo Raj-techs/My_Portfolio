72092464f84a72b6668b493c7af60c3933bbd710
Rajesh <puchakayalarajesh714@gmail.com>
2025-07-23 22:16:13 +0530

    img uploaded

--- src/app/Sections/Hero.jsx
+++ src/app/Sections/Hero.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useRef } from 'react';
 import styles from './Hero.module.css';
 import 'font-awesome/css/font-awesome.min.css';
 import Image from 'next/image';
-
+import MyImage from '../../../public/assets/rajeshImg.jpg'
 const Hero = () => {
   const textElementRef = useRef(null);
   const circleContainerRef = useRef(null);
@@ -126,7 +126,7 @@ const Hero = () => {
             </div>
             <div className={styles.profileImage}>
               <Image
-                src="https://media.istockphoto.com/id/1682296067/photo/happy-studio-portrait-or-professional-man-real-estate-agent-or-asian-businessman-smile-for.jpg?s=612x612&w=0&k=20&c=9zbG2-9fl741fbTWw5fNgcEEe4ll-JegrGlQQ6m54rg="
+                src={MyImage}
                 alt="Pucchakayala Rajesh Profile"
                 width={250}
                 height={250}
